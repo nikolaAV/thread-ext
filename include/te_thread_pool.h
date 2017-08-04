89b5fee9d5ae56535229da3c70d425c829be6c04
nikolaAV <alexander.nikolaenko@gmail.com>
2017-08-04 12:54:16 +0300

    thread_pool::update

--- include/te_thread_pool.h
+++ include/te_thread_pool.h
@@ -11,7 +11,9 @@
 
 
 #include "te_compiler_warning_suppress.h"
+#include <atomic>
 #include <utility>
+#include <memory>
 #include "te_compiler_warning_rollback.h"
 #include "te_compiler.h"
 
@@ -29,6 +31,49 @@ namespace thread_ex
    \example test_thread_pool.cpp
 */
 
+namespace tpis // thread_pool_internals
+{
+   class movable_function_body
+   {
+      struct void_signature
+      {
+         virtual void call() = 0;
+         virtual ~void_signature() {}
+      };
+
+      template <typename Callable>
+      struct void_signature_impl : void_signature
+      {
+         void call() override { f_(); }
+         void_signature_impl(Callable&& f) : f_(std::move(f)) {}
+         void_signature_impl& operator=(Callable&& f) { f_(std::move(f)); return *this; }
+
+      private:
+         Callable f_;
+      };
+
+   public:
+      void operator()() { f_->call(); }
+      template <typename Function>
+      movable_function_body(Function&& f) : f_{ new void_signature_impl<Function>(std::move(f)) } {}
+
+         // movable only
+      movable_function_body(movable_function_body&&)                  = default;
+      movable_function_body& operator=(movable_function_body&&)       = default;
+      movable_function_body(const movable_function_body&)             = delete;
+      movable_function_body& operator=(const movable_function_body&)  = delete;
+
+   private:
+      std::unique_ptr<void_signature> f_;
+   };
+}  // end of 'thread_pool_internals'
+
+class thread_pool
+{
+
+
+   std::atomic_bool done_;   
+};
 
 
 } // namespace thread_ex
